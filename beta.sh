fa87dae876dc4c7e740d98762c47617adbcacbd1
Goon <duy@wearetopgroup.com>
2023-07-27 13:03:37 +0700

    update beta release shell

--- beta.sh
+++ beta.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 TAG=v0
+CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+# delete old tag
 git checkout $TAG && git tag -d $TAG
 git push --delete origin $TAG
+
+# switch back to dev branch and release new tag
+git checkout $CUR_BRANCH
 git tag -a -m "beta release" $TAG
 git push origin $TAG
